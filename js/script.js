ac578d03913b9ad11f793fb9059ca02004bca5d6
Arianna Carotti <arianna.carotti@gmail.com>
2021-04-28 17:30:44 +0200

    provate a stampare le option della select

--- js/script.js
+++ js/script.js
@@ -1,18 +1,19 @@
 var app = new Vue({
     el: '#root',
     data: {
-        albumList : []
-                   
+        albumList : [],
+                          
     },
+    
     mounted() {
         axios
             .get('https://flynn.boolean.careers/exercises/api/array/music')
             .then((response) => {
-                this.albumList = response.data.response;
-            });
+                const result = response.data.response;              
+
+                this.albumList = result
                 
-                
-               
+            });               
        
     }
   })
